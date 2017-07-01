3b10fbf5a2d2b960d8737c7c0c4f9958769a2bf7
wy88y <1953421292@qq.com>
2017-07-01 18:03:12 +0800

    git gulp

--- gulpfile.js
+++ gulpfile.js
@@ -95,3 +95,59 @@ gulp.task('watch',function(){
 	gulp.watch(['dist/**'].on('change',livereload.changed);
 })；
 
+// 添加git tag
+var runSequence = require('run-sequence');
+var bump = require('gulp-bump');
+var gutil = require('gulp-util');
+var git = require('gulp-git');
+var fs = require('fs');
+
+
+gulp.task('bump-version', function () {
+  return gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({type: "patch"}).on('error', gutil.log))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+  return gulp.src('.')
+   .pipe(git.commit('[Prerelease] Bumped version number', {args: '-a'}));
+});
+
+gulp.task('push-changes', function (cb) {
+  git.push('origin', 'master', cb);
+});
+
+gulp.task('create-new-tag', function (cb) {
+  var version = getPackageJsonVersion();
+  git.tag(version, 'Created Tag for version: ' + version, function (error) {
+    if ( error) {
+	  return cb(error);
+	 }
+    git.push('origin', 'master', {args: '--tags'}, cb);
+  });
+  function getPackageJsonVersion () {
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  };
+});
+
+gulp.task('release', function (callback) {
+  runSequence(
+    'bump-version',
+	'commit-changes',
+	'push-changes',
+	'create-new-tag',
+     function (error) {
+	   if (error) {
+		  console.log(error.message);
+		} else {
+		  console.log('RELEASE FINISHED SUCCESSFULLY');
+	    }
+	   callback(error);
+	});
+});
+
+
+
+
+
